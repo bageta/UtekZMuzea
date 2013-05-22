2c6853614282b71b8a2b6c3df9afb958a5abaaa2
bageta <owde@seznam.cz>
2013-05-22 21:59:25 +0200

    dokumentace k tříde Position

--- src/helper/Position.java
+++ src/helper/Position.java
@@ -3,11 +3,21 @@ package helper;
 import com.jme3.math.Vector3f;
 
 /**
- *
- * @author Pavel
+ * Třída obsahující statické metody pro určování pozice,které se mohou hodit v různých
+ * místech kódu a nechceme aby byli závislé na konkrétní instanci nějaké třídy.
+ * @author Pavel Pilař
  */
 public class Position {
     
+    /**
+     * Metoda určující zda jsou dva objekty v určité vzdálenosti od sebe. V rámci
+     * os x a z. Kvůli výkonu se nepočítá vzdálenost, ale obě souřadnice musí
+     * být dostatečně blízko.
+     * @param position1 pozice prvního objektu
+     * @param position2 pozice druhého objektu
+     * @param diff požadovaná vzdálenost
+     * @return true, pokud jsou objekty dostatečně blízko, jinak false
+     */
     public static boolean isClose(Vector3f position1, Vector3f position2, float diff){
         return (Math.abs(position1.x-position2.x)<diff &&
                 Math.abs(position1.z - position2.z)<diff);
